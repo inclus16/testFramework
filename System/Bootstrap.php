5d1b385e4e43446eed70499de85fd9262689e775
Sergey <sergey.mindblast@mail.ru>
2022-11-23 00:25:21 +0400

    remove test skeleton

--- System/Bootstrap.php
+++ System/Bootstrap.php
@@ -4,20 +4,20 @@
 namespace System;
 
 
-use App\Providers\AppServiceProvider;
 use System\Http\Server;
 use System\InversionOfControl\ServiceProvider;
+use System\Providers\AppServiceProvider;
 use function Swoole\Coroutine\run;
 
 class Bootstrap
 {
-    private readonly ServiceProvider $appServiceProvider;
+    private readonly ServiceProvider $serviceProvider;
 
-    public function boot()
+
+    public function boot(AppServiceProvider $appServiceProvider)
     {
-        run(function () {
-            $services = new AppServiceProvider();
-            $this->appServiceProvider = $services->boot();
+        run(function () use ($appServiceProvider) {
+            $this->serviceProvider = $appServiceProvider->boot();
             $this->startServer();
         });
     }
@@ -25,7 +25,7 @@ class Bootstrap
     public function startServer(): void
     {
         /** @var Server $server */
-        $server = $this->appServiceProvider->getService(Server::class);
+        $server = $this->serviceProvider->getService(Server::class);
         $server->start();
     }
 }

2d8c32f59e674694a976a0042156c4f37ab656ae
Sergey <sergey.mindblast@mail.ru>
2022-11-23 20:08:07 +0400

    fix di

--- System/Log/PipelineLogger.php
+++ System/Log/PipelineLogger.php
@@ -3,13 +3,14 @@
 namespace System\Log;
 
 use System\AppContext;
+use System\Config\App\AppConfig;
 use System\Log\Abstraction\Logger;
 
 class PipelineLogger extends Logger
 {
 
-    public function __construct(AppContext $context)
+    public function __construct(AppContext $context, AppConfig $config)
     {
-        parent::__construct($context,'pipeline');
+        parent::__construct($context, $config, 'pipeline');
     }
 }
